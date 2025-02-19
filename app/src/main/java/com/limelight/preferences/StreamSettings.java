f10db7d55ea88bb3a008855735d5f80a7e535861
Silentree <aco_akku@hotmail.com>
2025-02-19 12:01:12 +0100

    Added a loop to capture all available displays and their resolution and framerate capabilities. It just works.

--- app/src/main/java/com/limelight/preferences/StreamSettings.java
+++ app/src/main/java/com/limelight/preferences/StreamSettings.java
@@ -26,6 +26,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.hardware.display.DisplayManager;
 
 import com.limelight.LimeLog;
 import com.limelight.PcView;
@@ -371,8 +372,7 @@ public class StreamSettings extends Activity {
                         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                             // Use the much nicer Display.getCutout() API on Android 10+
                             cutout = display.getCutout();
-                        }
-                        else {
+                        } else {
                             // Android 9 only
                             cutout = displayCutoutP;
                         }
@@ -418,11 +418,9 @@ public class StreamSettings extends Activity {
 
                         if ((width >= 3840 || height >= 2160) && maxSupportedResW < 3840) {
                             maxSupportedResW = 3840;
-                        }
-                        else if ((width >= 2560 || height >= 1440) && maxSupportedResW < 2560) {
+                        } else if ((width >= 2560 || height >= 1440) && maxSupportedResW < 2560) {
                             maxSupportedResW = 2560;
-                        }
-                        else if ((width >= 1920 || height >= 1080) && maxSupportedResW < 1920) {
+                        } else if ((width >= 1920 || height >= 1080) && maxSupportedResW < 1920) {
                             maxSupportedResW = 1920;
                         }
 
@@ -441,17 +439,15 @@ public class StreamSettings extends Activity {
                     if (avcDecoder != null) {
                         Range<Integer> avcWidthRange = avcDecoder.getCapabilitiesForType("video/avc").getVideoCapabilities().getSupportedWidths();
 
-                        LimeLog.info("AVC supported width range: "+avcWidthRange.getLower()+" - "+avcWidthRange.getUpper());
+                        LimeLog.info("AVC supported width range: " + avcWidthRange.getLower() + " - " + avcWidthRange.getUpper());
 
                         // If 720p is not reported as supported, ignore all results from this API
                         if (avcWidthRange.contains(1280)) {
                             if (avcWidthRange.contains(3840) && maxSupportedResW < 3840) {
                                 maxSupportedResW = 3840;
-                            }
-                            else if (avcWidthRange.contains(1920) && maxSupportedResW < 1920) {
+                            } else if (avcWidthRange.contains(1920) && maxSupportedResW < 1920) {
                                 maxSupportedResW = 1920;
-                            }
-                            else if (maxSupportedResW < 1280) {
+                            } else if (maxSupportedResW < 1280) {
                                 maxSupportedResW = 1280;
                             }
                         }
@@ -460,23 +456,21 @@ public class StreamSettings extends Activity {
                     if (hevcDecoder != null) {
                         Range<Integer> hevcWidthRange = hevcDecoder.getCapabilitiesForType("video/hevc").getVideoCapabilities().getSupportedWidths();
 
-                        LimeLog.info("HEVC supported width range: "+hevcWidthRange.getLower()+" - "+hevcWidthRange.getUpper());
+                        LimeLog.info("HEVC supported width range: " + hevcWidthRange.getLower() + " - " + hevcWidthRange.getUpper());
 
                         // If 720p is not reported as supported, ignore all results from this API
                         if (hevcWidthRange.contains(1280)) {
                             if (hevcWidthRange.contains(3840) && maxSupportedResW < 3840) {
                                 maxSupportedResW = 3840;
-                            }
-                            else if (hevcWidthRange.contains(1920) && maxSupportedResW < 1920) {
+                            } else if (hevcWidthRange.contains(1920) && maxSupportedResW < 1920) {
                                 maxSupportedResW = 1920;
-                            }
-                            else if (maxSupportedResW < 1280) {
+                            } else if (maxSupportedResW < 1280) {
                                 maxSupportedResW = 1280;
                             }
                         }
                     }
 
-                    LimeLog.info("Maximum resolution slot: "+maxSupportedResW);
+                    LimeLog.info("Maximum resolution slot: " + maxSupportedResW);
 
                     if (maxSupportedResW != 0) {
                         if (maxSupportedResW < 3840) {
@@ -514,8 +508,7 @@ public class StreamSettings extends Activity {
                         }
                         // Never remove 720p
                     }
-                }
-                else {
+                } else {
                     // We can get the true metrics via the getRealMetrics() function (unlike the lies
                     // that getWidth() and getHeight() tell to us).
                     DisplayMetrics metrics = new DisplayMetrics();
@@ -551,68 +544,67 @@ public class StreamSettings extends Activity {
                     // Never remove 30 FPS or 60 FPS
                 }
                 addNativeFrameRateEntry(maxSupportedFps);
-            }
 
-            // Android L introduces the drop duplicate behavior of releaseOutputBuffer()
-            // that the unlock FPS option relies on to not massively increase latency.
-            findPreference(PreferenceConfiguration.UNLOCK_FPS_STRING).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
-                @Override
-                public boolean onPreferenceChange(Preference preference, Object newValue) {
-                    // HACK: We need to let the preference change succeed before reinitializing to ensure
-                    // it's reflected in the new layout.
-                    final Handler h = new Handler();
-                    h.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            // Ensure the activity is still open when this timeout expires
-                            StreamSettings settingsActivity = (StreamSettings) SettingsFragment.this.getActivity();
-                            if (settingsActivity != null) {
-                                settingsActivity.reloadSettings();
+
+                // Android L introduces the drop duplicate behavior of releaseOutputBuffer()
+                // that the unlock FPS option relies on to not massively increase latency.
+                findPreference(PreferenceConfiguration.UNLOCK_FPS_STRING).setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                    @Override
+                    public boolean onPreferenceChange(Preference preference, Object newValue) {
+                        // HACK: We need to let the preference change succeed before reinitializing to ensure
+                        // it's reflected in the new layout.
+                        final Handler h = new Handler();
+                        h.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                // Ensure the activity is still open when this timeout expires
+                                StreamSettings settingsActivity = (StreamSettings) SettingsFragment.this.getActivity();
+                                if (settingsActivity != null) {
+                                    settingsActivity.reloadSettings();
+                                }
                             }
-                        }
-                    }, 500);
+                        }, 500);
 
-                    // Allow the original preference change to take place
-                    return true;
-                }
-            });
-
-            // Remove HDR preference for devices below Nougat
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
-                LimeLog.info("Excluding HDR toggle based on OS");
-                PreferenceCategory category =
-                        (PreferenceCategory) findPreference("category_advanced_settings");
-                category.removePreference(findPreference("checkbox_enable_hdr"));
-            }
-            else {
-                Display.HdrCapabilities hdrCaps = display.getHdrCapabilities();
-
-                // We must now ensure our display is compatible with HDR10
-                boolean foundHdr10 = false;
-                if (hdrCaps != null) {
-                    // getHdrCapabilities() returns null on Lenovo Lenovo Mirage Solo (vega), Android 8.0
-                    for (int hdrType : hdrCaps.getSupportedHdrTypes()) {
-                        if (hdrType == Display.HdrCapabilities.HDR_TYPE_HDR10) {
-                            foundHdr10 = true;
-                            break;
-                        }
+                        // Allow the original preference change to take place
+                        return true;
                     }
-                }
+                });
 
-                if (!foundHdr10) {
-                    LimeLog.info("Excluding HDR toggle based on display capabilities");
+                // Remove HDR preference for devices below Nougat
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                    LimeLog.info("Excluding HDR toggle based on OS");
                     PreferenceCategory category =
                             (PreferenceCategory) findPreference("category_advanced_settings");
                     category.removePreference(findPreference("checkbox_enable_hdr"));
-                }
-                else if (PreferenceConfiguration.isShieldAtvFirmwareWithBrokenHdr()) {
-                    LimeLog.info("Disabling HDR toggle on old broken SHIELD TV firmware");
-                    PreferenceCategory category =
-                            (PreferenceCategory) findPreference("category_advanced_settings");
-                    CheckBoxPreference hdrPref = (CheckBoxPreference) category.findPreference("checkbox_enable_hdr");
-                    hdrPref.setEnabled(false);
-                    hdrPref.setChecked(false);
-                    hdrPref.setSummary("Update the firmware on your NVIDIA SHIELD Android TV to enable HDR");
+                } else {
+                    Display.HdrCapabilities hdrCaps = display.getHdrCapabilities();
+
+                    // We must now ensure our display is compatible with HDR10
+                    boolean foundHdr10 = false;
+                    if (hdrCaps != null) {
+                        // getHdrCapabilities() returns null on Lenovo Lenovo Mirage Solo (vega), Android 8.0
+                        for (int hdrType : hdrCaps.getSupportedHdrTypes()) {
+                            if (hdrType == Display.HdrCapabilities.HDR_TYPE_HDR10) {
+                                foundHdr10 = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (!foundHdr10) {
+                        LimeLog.info("Excluding HDR toggle based on display capabilities");
+                        PreferenceCategory category =
+                                (PreferenceCategory) findPreference("category_advanced_settings");
+                        category.removePreference(findPreference("checkbox_enable_hdr"));
+                    } else if (PreferenceConfiguration.isShieldAtvFirmwareWithBrokenHdr()) {
+                        LimeLog.info("Disabling HDR toggle on old broken SHIELD TV firmware");
+                        PreferenceCategory category =
+                                (PreferenceCategory) findPreference("category_advanced_settings");
+                        CheckBoxPreference hdrPref = (CheckBoxPreference) category.findPreference("checkbox_enable_hdr");
+                        hdrPref.setEnabled(false);
+                        hdrPref.setChecked(false);
+                        hdrPref.setSummary("Update the firmware on your NVIDIA SHIELD Android TV to enable HDR");
+                    }
                 }
             }
 
